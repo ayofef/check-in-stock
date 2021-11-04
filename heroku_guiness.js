8ad61405d387a3d90750e115fa092aeeab4428ee
farouq.ayofe <farouq.ayofe@whenthen.com>
2021-11-04 19:28:18 +0000

    send telegram 10min

--- heroku_guiness.js
+++ heroku_guiness.js
@@ -1,3 +1,5 @@
 import { checkGuinessStoreHouse } from "./apis/guinessNitrosurge.js";
+import { sendMessage } from "./utils/telegram.js";
 
 checkGuinessStoreHouse();
+sendMessage("Backend job is still running in case you are wondering.. :) ", { disable_notification: true, dev: true });
